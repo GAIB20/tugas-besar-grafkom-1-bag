5687ab11be2d95e1a0335a8755c7e77a7c78740d
Mohammad Farhan Fahrezy <13521106@std.stei.itb.ac.id>
2024-03-19 19:08:51 +0700

    feat/draw/square

--- src/app.js
+++ src/app.js
@@ -2,67 +2,103 @@ var util = new Utility();
 var canvas = document.querySelector("#canvas");
 var gl = canvas.getContext("webgl");
 
-var shapes = {
-    lines: [],
-    squares: [],
-    rectangles: [],
-    polygons: [],
-}
+// var shapes = {
+//     lines: [],
+//     squares: [],
+//     rectangles: [],
+//     polygons: [],
+// }
+var shapes = [];
+var points = [];
+var selectStatus = false;
 
 var clickedModes = {
-    line: { click: false, hover: false },
-    square: { click: false, hover: false },
-    rectangle: { click: false, hover: false },
-    polygon: { click: false },
+  line: { click: false, hover: false },
+  square: { click: false, hover: false },
+  rectangle: { click: false, hover: false },
+  polygon: { click: false, hover: false },
+  select: false,
 };
 
 async function main() {
-    if (!gl) {
-        window.alert("Error initializing WebGL");
-        return;
+  if (!gl) {
+    window.alert("Error initializing WebGL");
+    return;
+  }
+
+  var program = await util.createDefaultShaderProgram(gl);
+  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  var resolutionUniformLocation = gl.getUniformLocation(
+    program,
+    "u_resolution"
+  );
+  var colorUniformLocation = gl.getAttribLocation(program, "a_color");
+  var vertexBuffer = gl.createBuffer();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+  // enabling the attribute so that we can take data from the buffer
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.enableVertexAttribArray(colorUniformLocation);
+
+  gl.vertexAttribPointer(
+    positionAttributeLocation,
+    2,
+    gl.FLOAT,
+    gl.FALSE,
+    6 * Float32Array.BYTES_PER_ELEMENT,
+    0
+  );
+  gl.vertexAttribPointer(
+    colorUniformLocation,
+    4,
+    gl.FLOAT,
+    gl.FALSE,
+    6 * Float32Array.BYTES_PER_ELEMENT,
+    2 * Float32Array.BYTES_PER_ELEMENT
+  );
+
+  drawScene();
+
+  function drawScene() {
+    util.resizeCanvasToDisplaySize(gl.canvas);
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // Clear the canvas
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Using the default program
+    gl.useProgram(program);
+
+    // Setting the resolution
+    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+    try {
+      shapes.forEach((s) => {
+        s.draw(gl);
+      });
+      points.forEach((p) => {
+        p.draw(gl);
+      });
+      // shapes.lines.forEach((l) => {
+      //     l.draw(gl);
+      // });
+      // shapes.squares.forEach((s) => {
+      //     s.draw(gl);
+      // });
+      // shapes.rectangles.forEach((r) => {
+      //     r.draw(gl);
+      // });
+      // shapes.polygons.forEach((p) => {
+      //     p.draw(gl);
+      // });
+    } catch (e) {
+      console.log(e.message);
     }
-
-    var program = await util.createDefaultShaderProgram(gl);
-    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-    var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
-    var colorUniformLocation = gl.getAttribLocation(program, "a_color");
-    var vertexBuffer = gl.createBuffer();
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    
-    // enabling the attribute so that we can take data from the buffer
-    gl.enableVertexAttribArray(positionAttributeLocation);
-    gl.enableVertexAttribArray(colorUniformLocation);
-
-    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 0);
-    gl.vertexAttribPointer(colorUniformLocation, 4, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
-
-    drawScene();
-
-    function drawScene() {
-        util.resizeCanvasToDisplaySize(gl.canvas);
-
-        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-        
-        // Clear the canvas
-        gl.clearColor(0, 0, 0, 0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        
-        // Using the default program
-        gl.useProgram(program);
-
-        // Setting the resolution
-        gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
-
-        try {
-            shapes.lines.forEach((l) => {
-                l.draw(gl);
-            });
-        } catch (e) {
-            console.log(e.message);
-        }
-        window.requestAnimationFrame(drawScene);
-    }
+    window.requestAnimationFrame(drawScene);
+  }
 }
 
 main();
--- src/tools/draw.js
+++ src/tools/draw.js
@@ -1,61 +1,197 @@
+var tempLine = null;
+var tempSquare = null;
 
 /**
  * Initializes line drawing mode by setting event handlers for mouse actions on the canvas.
  */
 function initializeLineDrawingMode() {
-    canvas.onmousedown = handleLineDrawClick;
-    canvas.onmousemove = handleLineDrawHover;
-    canvas.onmouseleave = resetLineDrawingState;
+  clearSelectedPoints();
+  canvas.onmousedown = handleLineDrawClick;
+  canvas.onmousemove = handleLineDrawHover;
+  canvas.onmouseleave = resetLineDrawingState;
 }
-  
+
 /**
  * Handles mouse click events on the canvas during line drawing mode.
- * 
+ *
  * @param {MouseEvent} event - The mouse event.
  */
 function handleLineDrawClick(event) {
-    const coordinates = util.getCanvasCoord(event);
-    const isFirstClick = !clickedModes.line.click;
-  
-    if (isFirstClick) {
-        var color = util.hexToRgba(document.getElementById("color-picker").value);
-        tempLine = new Line(gl, coordinates, color);
-        clickedModes.line.click = true;
-    } else {
-        resetLineDrawingState();
-    }
+  const coordinates = util.getCanvasCoord(event);
+  const isFirstClick = !clickedModes.line.click;
+
+  if (isFirstClick) {
+    var color = util.hexToRgba(document.getElementById("color-picker").value);
+    tempLine = new Line(gl, coordinates, color);
+    clickedModes.line.click = true;
+  } else {
+    resetLineDrawingState();
+  }
 }
 
 /**
  * Handles mouse move events on the canvas during line drawing mode, after a click.
- * 
+ *
  * @param {MouseEvent} event - The mouse event.
  */
 function handleLineDrawHover(event) {
-    const coordinates = util.getCanvasCoord(event);
-    
-    if (clickedModes.line.click) {
-        if (clickedModes.line.hover) {
-            // Update the endpoint of the last line drawn
-            const lastLineIndex = shapes.lines.length - 1;
-            shapes.lines[lastLineIndex].updateVertexCoordinates(coordinates, 1);
-        } else {
-            // Start a new line segment from the last clicked point
-            tempLine.appendVertex(coordinates);
-            shapes.lines.push(tempLine);
-            clickedModes.line.hover = true;
-        }
+  const coordinates = util.getCanvasCoord(event);
+
+  if (clickedModes.line.click) {
+    if (clickedModes.line.hover) {
+      // Update the endpoint of the last line drawn
+      // const lastLineIndex = shapes.lines.length - 1;
+      // shapes.lines[lastLineIndex].updateVertexCoordinates(coordinates, 1);
+      const lastIndex = shapes.length - 1;
+      shapes[lastIndex].updateVertexCoordinates(coordinates, 1);
+    } else {
+      // Start a new line segment from the last clicked point
+      tempLine.appendVertex(coordinates);
+      shapes.push(tempLine);
+      // shapes.lines.push(tempLine);
+      clickedModes.line.hover = true;
     }
+  }
 }
-  
+
 /**
  * Resets the drawing state when the mouse leaves the canvas during line drawing.
  */
 function resetLineDrawingState() {
-    if (clickedModes.line.click && clickedModes.line.hover) {
-        clickedModes.line.click = false;
-        clickedModes.line.hover = false;
-        
-        console.log(shapes.lines);
+  if (clickedModes.line.click && clickedModes.line.hover) {
+    clickedModes.line.click = false;
+    clickedModes.line.hover = false;
+
+    console.log(shapes);
+  }
+}
+
+/**
+ * Initializes rectangle drawing mode by setting event handlers for mouse actions on the canvas.
+ */
+function initializeRectangleDrawingMode() {
+  clearSelectedPoints();
+  canvas.onmousedown = handleRectangleDrawClick;
+  canvas.onmousemove = handleRectangleDrawHover;
+  canvas.onmouseleave = resetRectangleDrawingState;
+}
+
+/**
+ * Handles mouse click events on the canvas during rectangle drawing mode.
+ *
+ * @param {MouseEvent} event - The mouse event.
+ */
+function handleRectangleDrawClick(event) {
+  const coordinates = util.getCanvasCoord(event);
+  const isFirstClick = !clickedModes.rectangle.click;
+
+  if (isFirstClick) {
+    var color = util.hexToRgba(document.getElementById("color-picker").value);
+    tempRectangle = new Rectangle(gl, coordinates, color);
+    clickedModes.rectangle.click = true;
+  } else {
+    resetRectangleDrawingState();
+  }
+}
+
+/**
+ * Handles mouse move events on the canvas during rectangle drawing mode, after a click.
+ *
+ * @param {MouseEvent} event - The mouse event.
+ */
+function handleRectangleDrawHover(event) {
+  const coordinates = util.getCanvasCoord(event);
+
+  if (clickedModes.rectangle.click) {
+    if (clickedModes.rectangle.hover) {
+      // Update the endpoint of the last rectangle drawn
+      // const lastRectangleIndex = shapes.rectangles.length - 1;
+      // shapes.rectangles[lastRectangleIndex].changeLastCoord(coordinates);
+      const lastIndex = shapes.length - 1;
+      shapes[lastIndex].changeLastCoord(coordinates);
+    } else {
+      // Start a new rectangle segment from the last clicked point
+      tempRectangle.firstHoverCoord(coordinates);
+      shapes.push(tempRectangle);
+      // shapes.rectangles.push(tempRectangle);
+      clickedModes.rectangle.hover = true;
     }
+  }
+}
+
+/**
+ * Resets the drawing state when the mouse leaves the canvas during rectangle drawing.
+ */
+function resetRectangleDrawingState() {
+  if (clickedModes.rectangle.click && clickedModes.rectangle.hover) {
+    clickedModes.rectangle.click = false;
+    clickedModes.rectangle.hover = false;
+
+    console.log(shapes);
+  }
+}
+
+/**
+ * Initializes square drawing mode by setting event handlers for mouse actions on the canvas.
+ */
+function initializeSquareDrawingMode() {
+  clearSelectedPoints();
+  canvas.onmousedown = handleSquareDrawClick;
+  canvas.onmousemove = handleSquareDrawHover;
+  canvas.onmouseleave = resetSquareDrawingState;
+}
+
+/**
+ * Handles mouse click events on the canvas during square drawing mode.
+ *
+ * @param {MouseEvent} event - The mouse event.
+ */
+function handleSquareDrawClick(event) {
+  const coordinates = util.getCanvasCoord(event);
+  const isFirstClick = !clickedModes.square.click;
+
+  if (isFirstClick) {
+    var color = util.hexToRgba(document.getElementById("color-picker").value);
+    tempSquare = new Square(gl, coordinates, color);
+    clickedModes.square.click = true;
+  } else {
+    resetSquareDrawingState();
+  }
+}
+
+/**
+ * Handles mouse move events on the canvas during square drawing mode, after a click.
+ *
+ * @param {MouseEvent} event - The mouse event.
+ */
+function handleSquareDrawHover(event) {
+  const coordinates = util.getCanvasCoord(event);
+
+  if (clickedModes.square.click) {
+    if (clickedModes.square.hover) {
+      // Update the endpoint of the last rectangle drawn
+      // const lastRectangleIndex = shapes.rectangles.length - 1;
+      // shapes.rectangles[lastRectangleIndex].changeLastCoord(coordinates);
+      const lastIndex = shapes.length - 1;
+      shapes[lastIndex].changeLastCoord(coordinates);
+    } else {
+      // Start a new rectangle segment from the last clicked point
+      tempSquare.firstHoverCoord(coordinates);
+      shapes.push(tempSquare);
+      // shapes.rectangles.push(tempRectangle);
+      clickedModes.square.hover = true;
+    }
+  }
+}
+
+/**
+ * Resets the drawing state when the mouse leaves the canvas during square drawing.
+ */
+function resetSquareDrawingState() {
+  if (clickedModes.square.click && clickedModes.square.hover) {
+    clickedModes.square.click = false;
+    clickedModes.square.hover = false;
+
+    console.log(shapes);
+  }
 }
