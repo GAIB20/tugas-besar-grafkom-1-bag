cbfc58cb290a92e3345046e4875e7b57fb134898
farhanfahreezy <mohdfarhanfahrezy@gmail.com>
2024-03-19 19:19:10 +0700

    feat: delete & select

---
+++ src/tools/delete.js
@@ -0,0 +1,14 @@
+function deleteObject() {
+  if (selectStatus) {
+    shapes.splice(shapes.length - 1, 1);
+    selectStatus = false;
+    clearSelectedPoints();
+  }
+}
+
+function clearCanvas() {
+  shapes = [];
+
+  selectStatus = false;
+  clearSelectedPoints();
+}
