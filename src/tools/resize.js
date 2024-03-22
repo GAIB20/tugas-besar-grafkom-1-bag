3fb3284c5edaec3d2767a71e4273fb691372c000
bag <bagasaryo03@gmail.com>
2024-03-22 10:00:55 +0700

    feat/resize

---
+++ src/tools/resize.js
@@ -0,0 +1,37 @@
+function initializeResizeMode() {
+  canvas.onmousedown = handleResizeClick;
+  canvas.onmousemove = handleResizeMove;
+}
+
+function handleResizeClick(event) {
+  if (!selectStatus) return;
+
+  const coordinate = util.getCanvasCoord(event);
+  const isFirstClick = !clickedModes.resize;
+
+  if (isFirstClick) {
+    const countPoint = shapes[selectedIndex].getPointCount();
+
+    for (let i = 0; i < countPoint; i++) {
+      var pointStatus = shapes[selectedIndex].isPointWithinMargin(coordinate);
+      if (pointStatus.isEndPoint) {
+        selectedPoint = pointStatus.pointIndex;
+        clickedModes.resize = true;
+        break;
+      }
+    }
+  } else {
+    resetSelectedPoints();
+    clickedModes.resize = false;
+  }
+}
+
+function handleResizeMove(event) {
+  if (!selectStatus) return;
+
+  const coordinate = util.getCanvasCoord(event);
+  if (clickedModes.resize) {
+    shapes[selectedIndex].resize(selectedPoint, coordinate);
+    resetSelectedPoints();
+  }
+}
